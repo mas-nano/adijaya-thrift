628034a45e46da80e0712871bf548d925a9898cb
mas-nano <nabilislam8@gmail.com>
2021-12-23 12:41:32 +0700

    minor fix 0.2.12

--- app/Http/Controllers/ProdukAPI.php
+++ app/Http/Controllers/ProdukAPI.php
@@ -19,18 +19,35 @@ class ProdukAPI extends Controller
      */
     public function index(User $user, Request $request)
     {
-        $produk = Produk::where('stok', '>', 0)->take($request->take)->get();
-        if(!is_null($kategori = $user->pemesanan->last())){
-            if($kategori = $kategori->produk->kategori){
-                if(count($produk = Produk::where('kategori', $kategori)->where('stok', '>', 0)->take($request->take)->get())==0){
-                    $produk = Produk::where('stok', '>', 0)->take($request->take)->get();
+        $produk = Produk::where("stok", ">", 0)
+            ->take($request->take)
+            ->get();
+        if (!is_null($kategori = $user->pemesanan->last())) {
+            if ($kategori = $kategori->produk->kategori) {
+                if (
+                    count(
+                        $produk = Produk::where("kategori", $kategori)
+                            ->where("stok", ">", 0)
+                            ->take($request->take)
+                            ->get()
+                    ) == 0
+                ) {
+                    $produk = Produk::where("stok", ">", 0)
+                        ->take($request->take)
+                        ->get();
                 }
             }
         }
         foreach ($produk as $p) {
-            $p->url = app('firebase.storage')->getBucket()->object("img/produk/".$p->foto)->signedUrl(new \DateTime('tomorrow'));
+            $p->url = app("firebase.storage")
+                ->getBucket()
+                ->object("img/produk/" . $p->foto)
+                ->signedUrl(new \DateTime("tomorrow"));
         }
-        return response()->json(['status'=>200, 'produk'=>$produk], Response::HTTP_OK);
+        return response()->json(
+            ["status" => 200, "produk" => $produk],
+            Response::HTTP_OK
+        );
     }
 
     /**
@@ -40,17 +57,35 @@ class ProdukAPI extends Controller
      */
     public function get(Request $request)
     {
-        try{
-            $data = json_decode(Produk::filter(request(['search', 'daerah', 'kategori', 'sort', 'promo', 'min', 'max']))->where('stok', '>', 0)->take($request->take)->get(), true);
-            for($i=0;$i<count($data);$i++){
-                $data[$i]['harga']=number_format($data[$i]['harga'],0,',','.');
-                if($data[$i]['promo']!=null){
-                    $data[$i]['promo']=number_format($data[$i]['promo'],0,',','.');
-                }
-                $data[$i]['url'] = app('firebase.storage')->getBucket()->object("img/produk/".$data[$i]['foto'])->signedUrl(new \DateTime('tomorrow'));
+        try {
+            $data = json_decode(
+                Produk::filter(
+                    request([
+                        "search",
+                        "daerah",
+                        "kategori",
+                        "sort",
+                        "promo",
+                        "min",
+                        "max",
+                    ])
+                )
+                    ->where("stok", ">", 0)
+                    ->take($request->take)
+                    ->get(),
+                true
+            );
+            for ($i = 0; $i < count($data); $i++) {
+                $data[$i]["url"] = app("firebase.storage")
+                    ->getBucket()
+                    ->object("img/produk/" . $data[$i]["foto"])
+                    ->signedUrl(new \DateTime("tomorrow"));
             }
-            return response()->json(['status'=>200, 'produk'=>$data], Response::HTTP_OK);
-        }catch(QueryException $e){
+            return response()->json(
+                ["status" => 200, "produk" => $data],
+                Response::HTTP_OK
+            );
+        } catch (QueryException $e) {
             return $e->errorInfo;
         }
     }
@@ -74,17 +109,31 @@ class ProdukAPI extends Controller
      */
     public function show(Produk $produk, Request $request)
     {
-        if($request->user_id){
-            $wishlist = Wishlist::where('user_id', $request->user_id)->where('produk_id', $produk->id)->first();
+        if ($request->user_id) {
+            $wishlist = Wishlist::where("user_id", $request->user_id)
+                ->where("produk_id", $produk->id)
+                ->first();
             $produk->user->review;
-            $produk->url = app('firebase.storage')->getBucket()->object("img/produk/".$produk->foto)->signedUrl(new \DateTime('tomorrow'));
-            $produk->user->url = app('firebase.storage')->getBucket()->object("img/profil/".$produk->user->photo)->signedUrl(new \DateTime('tomorrow'));
-            return response()->json(['status'=>200,'produk'=>$produk, 'wishlist'=>$wishlist], Response::HTTP_OK);
+            $produk->url = app("firebase.storage")
+                ->getBucket()
+                ->object("img/produk/" . $produk->foto)
+                ->signedUrl(new \DateTime("tomorrow"));
+            $produk->user->url = app("firebase.storage")
+                ->getBucket()
+                ->object("img/profil/" . $produk->user->photo)
+                ->signedUrl(new \DateTime("tomorrow"));
+            return response()->json(
+                ["status" => 200, "produk" => $produk, "wishlist" => $wishlist],
+                Response::HTTP_OK
+            );
         }
-        return response()->json([
-            'status'=>400,
-            'message' => 'Masukkan parameter user_id'
-        ], Response::HTTP_BAD_REQUEST);
+        return response()->json(
+            [
+                "status" => 400,
+                "message" => "Masukkan parameter user_id",
+            ],
+            Response::HTTP_BAD_REQUEST
+        );
     }
 
     /**
@@ -95,18 +144,40 @@ class ProdukAPI extends Controller
      */
     public function search(Request $request)
     {
-        if($request['take']){
-            $produk = json_decode(Produk::filter(request(['search', 'daerah', 'kategori', 'sort', 'promo', 'min', 'max']))->take($request['take'])->get());
-            for($i=0;$i<count($produk);$i++){
-                $produk[$i]->harga=number_format($produk[$i]->harga,0,',','.');
-                $produk[$i]->url = app('firebase.storage')->getBucket()->object("img/produk/".$produk[$i]->foto)->signedUrl(new \DateTime('tomorrow'));
+        if ($request["take"]) {
+            $produk = json_decode(
+                Produk::filter(
+                    request([
+                        "search",
+                        "daerah",
+                        "kategori",
+                        "sort",
+                        "promo",
+                        "min",
+                        "max",
+                    ])
+                )
+                    ->take($request["take"])
+                    ->get()
+            );
+            for ($i = 0; $i < count($produk); $i++) {
+                $produk[$i]->url = app("firebase.storage")
+                    ->getBucket()
+                    ->object("img/produk/" . $produk[$i]->foto)
+                    ->signedUrl(new \DateTime("tomorrow"));
             }
-            return response()->json(['status'=>200,'produk'=>$produk], Response::HTTP_OK);
-        }else{
-            return response()->json([
-                'status'=>400,
-                'message' => 'Masukkan parameter "take"'
-            ],Response::HTTP_BAD_REQUEST);
+            return response()->json(
+                ["status" => 200, "produk" => $produk],
+                Response::HTTP_OK
+            );
+        } else {
+            return response()->json(
+                [
+                    "status" => 400,
+                    "message" => 'Masukkan parameter "take"',
+                ],
+                Response::HTTP_BAD_REQUEST
+            );
         }
     }
 
